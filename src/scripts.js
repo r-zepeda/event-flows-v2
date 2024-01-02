c093c7a871b2a005f358f15316ffbbc760475c9c
r-zepeda <ralphpzepeda@gmail.com>
2024-01-02 15:18:01 +0800

    Upload Files

---
+++ src/scripts.js
@@ -0,0 +1,51 @@
+const textElement = document.getElementById('changingText');
+const words = ['น่ารื่นรมย์', 'delightful'];
+let currentIndex = 0;
+
+function changeText() {
+  textElement.style.opacity = 0;
+  setTimeout(() => {
+    textElement.textContent = words[currentIndex];
+    currentIndex = (currentIndex + 1) % words.length;
+    textElement.style.opacity = 1;
+  }, 500); // Delay the text change to match the transition duration
+}
+
+setInterval(changeText, 4000); // Change text every 2 seconds
+
+
+function openTab(tabId,tabIds) {
+  // Hide all tab content
+  document.querySelectorAll('.tab-content').forEach(content => {
+    content.style.display = 'none';
+  });
+
+  // Deactivate all tabs
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  // Show the selected tab content
+  document.getElementById(tabId).style.display = 'flex';
+
+  // Activate the selected tab
+  document.querySelector(`.tab[id="${tabIds}"]`).classList.add('active');
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the current page filename (e.g., "about.html" or "index.html")
+  var currentPage = window.location.pathname.split('/').pop();
+
+  // Remove the ".html" extension if present
+  currentPage = currentPage.replace('.html', '');
+
+  // Get the corresponding navigation link element
+  var currentNavLink = document.getElementById(currentPage);
+
+  // If the navigation link element exists, add the "active" class
+  if (currentNavLink) {
+    currentNavLink.classList.add('active');
+  }
+});
