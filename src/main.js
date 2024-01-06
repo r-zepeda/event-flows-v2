7ff920b2deccb122f9010a4523386d7e898dec99
r-zepeda <ralphpzepeda@gmail.com>
2024-01-06 12:21:33 +0800

    Removed Scripts.js
    Transferred functions from scripts.js to main.
    -Has Bugs (Tabs doesnt work index)

--- src/main.js
+++ src/main.js
@@ -7,3 +7,61 @@ import '../styles/main_services.css'
 import '../styles/main_events.css'
 import '../styles/main_partners.css'
 import '../styles/about_hero.css'
+// import './scripts'
+
+
+const textElement = document.getElementById('changingText');
+const words = ['น่ารื่นรมย์', 'delightful'];
+let currentIndex = 0;
+
+function changeText() {
+  textElement.style.opacity = 0;
+  setTimeout(() => {
+    textElement.textContent = words[currentIndex];
+    currentIndex = (currentIndex + 1) % words.length;
+    textElement.style.opacity = 1;
+  }, 500); // Delay the text change to match the transition duration
+}
+
+setInterval(changeText, 4000); // Change text every 2 seconds
+
+
+function openTab(tabId,tabIds) {
+  // Hide all tab content
+  document.querySelectorAll('.tab-content').forEach(content => {
+    content.style.display = 'none';
+  });
+
+  // Deactivate all tabs
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
+
+  // Show the selected tab content
+  document.getElementById(tabId).style.display = 'flex';
+
+  // Activate the selected tab
+  document.querySelector(`.tab[id="${tabIds}"]`).classList.add('active');
+}
+
+// var myButton = document.getElementById('tab2s');
+// myButton.addEventListener('click', openTab);
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the current page filename (e.g., "about.html" or "index.html")
+  var currentPage = window.location.pathname.split('/').pop();
+
+  // Remove the ".html" extension if present
+  currentPage = currentPage.replace('.html', '');
+
+  // Get the corresponding navigation link element
+  var currentNavLink = document.getElementById(currentPage);
+
+  // If the navigation link element exists, add the "active" class
+  if (currentNavLink) {
+    currentNavLink.classList.add('active');
+  }
+});
+
