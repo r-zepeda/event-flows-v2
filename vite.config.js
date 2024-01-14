622541d244697e2e68590ab7dd6200c0b78f53ff
r-zepeda <ralphpzepeda@gmail.com>
2024-01-15 00:42:38 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        portfolio: resolve(__dirname, 'portfolio.html'),
+        service1: resolve(__dirname, 'services_1.html'),
+        services: resolve(__dirname, 'services.html'),
         test: resolve(__dirname, 'test.html'),
       },
     },
